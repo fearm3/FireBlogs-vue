bf7c231af95c29ec552b81575031b9524cb3a5c8
fearm3 <ibraaa1628@gmail.com>
2022-01-22 00:56:19 +0300

    add BlogPost

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,41 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost
+      :post="post"
+      v-for="(post, index) in sampleBlogPost"
+      :key="index"
+    />
+  </div>
 </template>
 
 <script>
+import BlogPost from "../components/BlogPost";
 export default {
   name: "Home",
-  components: {},
+  components: { BlogPost },
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome!",
+        blogPost:
+          "Weekly blog articles with all things programming including HTML,CSS, Javascript and more. Register today to never miss a post",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      sampleBlogPost: [
+        {
+          title: "This is a Filler Title!",
+          blogHTML: "This is a filler blog post title!",
+          blogCoverPhoto: "beautiful-stories",
+        },
+        {
+          title: "This is a Filler Title!",
+          blogHTML: "This is a filler blog post title!",
+          blogCoverPhoto: "designed-for-everyone",
+        },
+      ],
+    };
+  },
 };
 </script>
