36b0bb2365dba35f3cd7f138a3df8c91cc26c402
fearm3 <ibraaa1628@gmail.com>
2022-01-29 19:43:00 +0300

    add login and register

---
+++ src/views/ForgotPassword.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="reset-password">
+    <div class="form-wrap">
+      <form class="reset">
+        <h2>Reset password</h2>
+        <p>Forgot yout password? Enter your email to reset it</p>
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="Email" v-model="email" />
+            <email class="icon" />
+          </div>
+        </div>
+        <button>Reset</button>
+        <div class="angle"></div>
+      </form>
+      <div class="background"></div>
+    </div>
+  </div>
+</template>
+<script>
+import email from "../assets/Icons/envelope-regular.svg";
+export default {
+  name: "ForgotPassword",
+  components: {
+    email,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.reset-password {
+  position: relative;
+  .form-wrap {
+    .reset {
+      h2 {
+        margin-bottom: 8px;
+      }
+      p {
+        text-align: center;
+        margin-bottom: 32px;
+      }
+    }
+  }
+}
+</style>
